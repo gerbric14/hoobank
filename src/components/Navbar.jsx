03f9c740d5f1e0cc1ca9aa61c63ee48ce8a97bf9
German Briceno <gerbric14@gmail.com>
2024-03-04 17:03:57 -0500

    Modificaciones al menu movil

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,8 +15,8 @@ const NavBar = () => {
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] ${
-              active === nav.title ? "text-white" : "text-dimWhite"
+            className={`font-poppins font-normal cursor-pointer text-[16px] hover:text-white ${
+              active === nav.title ? "text-cyan-400" : "text-dimWhite"
             } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
             onClick={() => setActive(nav.title)}
           >
@@ -39,18 +39,25 @@ const NavBar = () => {
         <div
           className={`${
             toggle ? "flex" : "hidden"
-          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[200px] rounded-xl`}
+          } p-6 bg-black-gradient-2 h-screen z-[6] absolute top-20 right-0 left-0 mx-4 my-2 min-w-[200px] rounded-xl`}
         >
-          <ul className="list-none flex flex-col justify-end items-center flex-1">
+          <ul className="list-none flex flex-col justify-center items-center flex-1">
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
-                className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                  active === nav.title ? "text-white" : "text-dimWhite"
+                className={`font-poppins font-medium cursor-pointer text-[16px] hover:text-white hover:bg-gray-500 border hover:border-gray-500 p-4 rounded-xl w-32  ${
+                  active === nav.title ? "text-cyan-400" : "text-dimWhite"
                 } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
                 onClick={() => setActive(nav.title)}
               >
-                <a href={`#${nav.id}`}>{nav.title}</a>
+                <a
+                  href={`#${nav.id}`}
+                  onClick={() => {
+                    setToggle((prev) => !prev);
+                  }}
+                >
+                  {nav.title}
+                </a>
               </li>
             ))}
           </ul>
